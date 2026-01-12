0fbd5808950b07be2e466a3acd2801236c83bac1
qiu <1753638325@qq.com>
2026-01-12 16:09:39 +0800

    rpc请求响应对象

--- src/main/java/com/qiu/rpc/model/RpcRequest.java
+++ src/main/java/com/qiu/rpc/model/RpcRequest.java
@@ -1,5 +1,12 @@
 package com.qiu.rpc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author qiu
  * @version 1.0
@@ -9,7 +16,19 @@ package com.qiu.rpc.model;
  * @date 2026/1/12 16:04
  * @since 1.0
  */
-public class RpcRequest {
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RpcRequest implements Serializable {
 
+    private static final long serialVersionUID = 31687997067227778L;
 
+    private String serviceName;
+
+    private String methodName;
+
+    private Class<?>[] parameterTypes;
+
+    private Object[] parameters;
 }
--- src/main/java/com/qiu/rpc/model/RpcResponse.java
+++ src/main/java/com/qiu/rpc/model/RpcResponse.java
@@ -1,5 +1,10 @@
 package com.qiu.rpc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author qiu
  * @version 1.0
@@ -9,5 +14,17 @@ package com.qiu.rpc.model;
  * @date 2026/1/12 16:04
  * @since 1.0
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RpcResponse {
+
+    private Object data;
+
+    private Class<?> dataType;
+
+    private String message;
+
+    private Exception exception;
 }
